60ad30f24aff189f3e736883ddf5f2c6e688b4d9
Thomas Bringer <thomas.bringer@student-cs.fr>
2025-04-05 15:01:11 +0200

    eyes

---
+++ game/mole/code/eye_pivots.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@onready var head_pivot: Node2D = $"../Points/Followers/Head/Scaler/HeadPivot"
+
+@onready var eye1_pivot: Node2D = $"../Points/Followers/Head/Scaler/Eye1BasePivot"
+@onready var eye2_pivot: Node2D = $"../Points/Followers/Head/Scaler/Eye2BasePivot"
+
+@onready var eye1: Sprite2D = $"../Visuals/CircleHeadMask/Eye1Visual"
+@onready var eye2: Sprite2D = $"../Visuals/CircleHeadMask/Eye2Visual"
+
+const SPREAD_DEGREES: float = 35
+const TOLERANCE: float = .1
+
+func _ready() -> void:
+	process_priority = head_pivot.process_priority + 1
+
+func norm_angle(angle_degrees: float) -> float:
+	return (fposmod(angle_degrees + 180., 360.) - 180.)
+
+func _process(delta: float) -> void:
+	var angle: float = norm_angle(head_pivot.rotation_degrees)	
+	var mid_angle: float = (
+		lerp(SPREAD_DEGREES, 180. - SPREAD_DEGREES, angle / 180.)
+		if angle >= 0
+		else lerp(SPREAD_DEGREES, - 180 - SPREAD_DEGREES, - angle / 180.)
+	)
+	apply_angle(eye1_pivot, mid_angle - SPREAD_DEGREES, eye1)
+	apply_angle(eye2_pivot, mid_angle + SPREAD_DEGREES, eye2)
+
+func is_pointing_front(pivot: Node2D) -> bool:
+	var dir = pivot.global_transform.x
+	return dir.dot(Vector2.DOWN) >= -TOLERANCE
+
+func order(pivot: Node2D, node: Node2D, z: int) -> void:
+	node.z_index = z if is_pointing_front(pivot) else -z
+
+func apply_angle(eye_pivot: Node2D, angle: float, eye: Node2D) -> void:
+	eye_pivot.rotation_degrees = angle
+	order(eye_pivot, eye, 15)
--- game/mole/z_ordering.gd
+++ game/mole/z_ordering.gd
@@ -4,10 +4,24 @@ extends Node2D
 @onready var nose: Line2D = $"../Visuals/Nose"
 const TOLERANCE: float = .1
 
+#@onready var eye1_pivot: Node2D = $"../Points/Followers/Head/Scaler/HeadPivot/Eye1BasePivot"
+#@onready var eye2_pivot: Node2D = $"../Points/Followers/Head/Scaler/HeadPivot/Eye2BasePivot"
+#
+#@onready var eye1: Sprite2D = $"../Visuals/Eye1Visual"
+#@onready var eye2: Sprite2D = $"../Visuals/Eye2Visual"
+
 func _ready() -> void:
 	process_priority = head_pivot.process_priority + 1
 
+func is_pointing_front(pivot: Node2D) -> bool:
+	var dir = pivot.global_transform.x
+	return dir.dot(Vector2.DOWN) >= -TOLERANCE
+
+func order(pivot: Node2D, node: Node2D, z: int) -> void:
+	node.z_index = z if is_pointing_front(pivot) else -z
+
 func _process(delta: float) -> void:
-	var dir = head_pivot.transform.x
-	var is_front: bool = dir.dot(Vector2.DOWN) >= -TOLERANCE
-	nose.z_index = 5 if is_front else -5
+	#print(eye1_pivot.transform.x.dot(Vector2.DOWN))
+	order(head_pivot, nose, 1)
+	#order(eye1_pivot, eye1, 2)
+	#order(eye2_pivot, eye2, 2)
